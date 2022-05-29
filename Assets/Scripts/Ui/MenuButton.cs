39e47b38b3c5c8d88dcfa8a67dec3a436e53284d
wagren <silvacostawagner@gmail.com>
2022-05-29 20:49:16 -0300

    menu buttons and animations

---
+++ Assets/Scripts/Ui/MenuButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public Animator anim;
+    private bool isIdle = false;
+
+    public void EnableAnima()
+    {
+        anim.SetBool("isIdle", false);
+        isIdle = true;
+    }
+    public void DisableAnima()
+    {
+        anim.SetBool("isIdle", true);
+        isIdle = false;
+    }
+}
