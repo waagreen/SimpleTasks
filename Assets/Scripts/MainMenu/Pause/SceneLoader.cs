3a9ebfcf312a247dcbd8583c0a3eeee0f9cbcead
Daniel Bisaggio <danniells.bisaggio@gmail.com>
2022-06-02 19:37:22 -0300

    no message

--- Assets/Scripts/MainMenu/Pause/SceneLoader.cs
+++ Assets/Scripts/MainMenu/Pause/SceneLoader.cs
@@ -87,4 +87,16 @@ public class SceneLoader : MonoBehaviour
         Time.timeScale = 1f;
         Application.Quit();
     }
+
+    public void SaveVolumeButton(){
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+
+    void LoadValues(){
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
 }
